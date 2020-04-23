545577b80279dca55b9f5b37cec164242c48497b
Guillermo Roditi Dominguez <groditi@gmail.com>
2020-04-23 15:55:36 -0700

    Create bloomberg_api_get_daily_data.r
    bloomberg API data acquisition

---
+++ bloomberg_api_get_daily_data.r
@@ -0,0 +1,12 @@
+library("seasonal")
+library(Rblpapi)
+library("xts")
+
+bbgconn <- blpConnect()
+
+monthlyOptions <- structure(c("ACTUAL", "DAILY"), names = c("periodicityAdjustment", "periodicitySelection"))
+result = bdh("USCBFTWI Index", "PX_LAST", start.date=as.Date("2014-01-01"), con=bbgconn, options=monthlyOptions)
+xts_daily_result = xts(result[,2], order.by = as.Date(result[,1]))
+names(xts_daily_result) = c("deposit.amount")
+
+write.zoo(xts_daily_result, file = "./etc/withholding-daily-NSA.csv", sep=",")
